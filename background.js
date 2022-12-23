f3c05cffd0143d31db3950171292d52fa700b137
Edgar Laz <edgarguitarist@gmail.com>
2022-12-23 01:12:01 -0500

    Restoring onInstalled extension Action

--- background.js
+++ background.js
@@ -1,8 +1,4 @@
 chrome.runtime.onInstalled.addListener(({ reason, version }) => {
-  console.log("onInstalled", reason, version)
-  if (reason === "install") {
-    chrome.storage.sync.set({ version });
-    let url = chrome.runtime.getURL("index.html");
-    chrome.tabs.create({ url });
-  }
+  let url = chrome.runtime.getURL("index.html");
+  chrome.tabs.create({ url });
 });
