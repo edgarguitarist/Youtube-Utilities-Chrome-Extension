00061e16e70e3bc2f5f6f36d00330afa1ea24272
edgarguitarist <edgarguitarist@gmail.com>
2023-10-22 20:01:25 -0500

    feat: listening changes

--- load.js
+++ load.js
@@ -1,7 +1,7 @@
 function getLocalStorage(callback) {
-    chrome.storage.local.get(['yt-shortcuts'], function(result) {
+    chrome.storage.local.get(['yt-shortcuts'], function (result) {
         const data = result['yt-shortcuts'] || {
-            newButton: false, 
+            newButton: false,
             OAChecker: false,
             DarkMode: false,
         };
@@ -10,7 +10,7 @@ function getLocalStorage(callback) {
 }
 
 function setLocalStorage(data, callback) {
-    chrome.storage.local.set({'yt-shortcuts': data}, function() {
+    chrome.storage.local.set({ 'yt-shortcuts': data }, function () {
         console.log('Datos guardados en chrome.storage.local');
         if (callback) {
             callback();
@@ -19,15 +19,15 @@ function setLocalStorage(data, callback) {
 }
 
 function getDarkMode(callback) {
-    getLocalStorage(function(data) {
+    getLocalStorage(function (data) {
         callback(data.DarkMode);
     });
 }
 
-function setDarkMode(callback) {
-    getLocalStorage(function(data) {
-        data.DarkMode = !data.DarkMode;
-        setLocalStorage(data, function() {
+function setDarkMode(isDarkMode, callback) {
+    getLocalStorage(function (data) {
+        data.DarkMode = isDarkMode;
+        setLocalStorage(data, function () {
             if (callback) {
                 callback(data.DarkMode);
             }
@@ -38,19 +38,27 @@ function setDarkMode(callback) {
 const body = document.getElementsByTagName('body')[0];
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 
-//si existe darkModeToggle, entonces estamos en la página de opciones
 if (darkModeToggle !== null) {
-    darkModeToggle.addEventListener('change', function() {
-        setDarkMode(function(isDarkMode) {
+    darkModeToggle.addEventListener('change', function () {
+        setDarkMode(darkModeToggle.checked, function (isDarkMode) {
             darkModeToggle.checked = isDarkMode;
         });
     });
-    
-    body.onload = function() {
+
+    body.onload = function () {
         console.log('load');
-        getDarkMode(function(isDarkMode) {
+        getDarkMode(function (isDarkMode) {
             darkModeToggle.checked = isDarkMode;
         });
-    };    
-}
+    };
 
+    // Escuchar cambios en chrome.storage.local
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
+        if (namespace === 'local' && changes['yt-shortcuts']) {
+            const newValues = changes['yt-shortcuts'].newValue;
+            if (newValues.DarkMode !== undefined) {
+                darkModeToggle.checked = newValues.DarkMode;
+            }
+        }
+    });
+}
