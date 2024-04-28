a6a5fc86d34f4f2eb1d5904ebaf834a261d5658f
--get <edgarguitarist@gmail.com>
2024-04-28 07:41:53 -0500

    Refactor toggleIcon function and update initial values in window.onload

--- features.js
+++ features.js
@@ -1,147 +1,148 @@
-const conf = {
+const DATA_BUTTONS = {
   like: {
     id: "img_like_yt",
     icon_pressed: "mdi:thumb-up.svg",
     icon: "mdi:thumb-up-outline.svg",
-    other: "dislike"
+    other: "dislike",
+    key: "d"
   },
   dislike: {
     id: "img_dislike_yt",
     icon_pressed: "mdi:thumb-down.svg",
     icon: "mdi:thumb-down-outline.svg",
-    other: "like"
+    other: "like",
+    key: "a"
   }
 };
 
+const CURRENT_SECONDS = 3;
 
 
 window.onload = () => {
   setTimeout(function () {
-    chrome.storage.local.get(['yt-shortcuts'], function (result) {
-      const data = result['yt-shortcuts'] || {      
-        DarkMode: true,
-        Lang: 'en'
-      };
+    function checkNoFocus() {
+      return (
+        document.activeElement.tagName !== "INPUT" &&
+        document.activeElement.tagName !== "TEXTAREA" &&
+        document.activeElement.id !== "search" &&
+        document.activeElement.id !== "contenteditable-root"
+      );
+    }
 
-
-      function checkNoFocus() {
-        return (
-          document.activeElement.tagName !== "INPUT" &&
-          document.activeElement.tagName !== "TEXTAREA" &&
-          document.activeElement.id !== "search" &&
-          document.activeElement.id !== "contenteditable-root"
-        );
-      }
-
-      function getButtons() {
-        let wrapper = document.querySelector('.YtSegmentedLikeDislikeButtonViewModelSegmentedButtonsWrapper')
-        if (!wrapper) return { buttonLike: null, buttonDisLike: null }
-        wrapper = Array.from(wrapper?.childNodes)
-        const [buttonLike, buttonDisLike] = wrapper.map((e) => e
+    function getButtons() {
+      let wrapper = document.querySelector('.YtSegmentedLikeDislikeButtonViewModelSegmentedButtonsWrapper')
+      if (!wrapper) return { buttonLike: null, buttonDisLike: null }
+      wrapper = Array.from(wrapper?.childNodes)
+      const [buttonLike, buttonDisLike] = wrapper.map((e) => e
         .querySelector('toggle-button-view-model > button-view-model > button'))
-        return {
-          buttonLike,
-          buttonDisLike,
-        }
+      return {
+        buttonLike,
+        buttonDisLike,
       }
+    }
 
-      function getAriaPressed(button) {
-        return button.getAttribute("aria-pressed") === "true";
-      }
+    function getAriaPressed(button) {
+      return button?.getAttribute("aria-pressed") === "true";
+    }
 
-      createButton = (name, newButton, original, key) => {
+    createButton = (name, newButton, original) => {
 
-        const img = document.createElement("img");
+      const ICON = document.createElement("img");
 
-        let icon = getAriaPressed(original) ? conf[name].icon_pressed : conf[name].icon;
+      let icon = getAriaPressed(original) ? DATA_BUTTONS[name].icon_pressed : DATA_BUTTONS[name].icon;
 
-        img.src = `https://api.iconify.design/${icon}`;
-        img.id = conf[name].id;
-        img.style = "filter: invert(1); width: inherit;";
-        
-        let button = newButton.cloneNode(true);
+      ICON.src = `https://api.iconify.design/${icon}`;
+      ICON.id = DATA_BUTTONS[name].id;
+      ICON.style = "filter: invert(1); width: inherit;";
 
-        button.removeChild(button.firstChild);
-        button.appendChild(img);
+      let button = newButton.cloneNode(true);
 
-        button.id = `newButtons-${name}`;
-        button.style = "width: 24px; position: relative; top: -12px; margin-right: 20px;";
-        button.classList.remove("ytp-fullscreen-button");
-        button.setAttribute(
-          "title",
-          `${name[0].toUpperCase() + name.slice(1)}`
-        );
-        button.setAttribute("aria-label", name);
-        button.setAttribute("data-title-no-tooltip", name);
-        button.setAttribute("aria-keyshortcuts", key);
+      button.removeChild(button.firstChild);
+      button.appendChild(ICON);
 
-        button.addEventListener("click", () => {
-          console.log(name, original);
-          let icon = getAriaPressed(original) ? conf[name].icon : conf[name].icon_pressed;
-          img.src = `https://api.iconify.design/${icon}`;
-          oppositeButton = document.getElementById(`newButtons-${conf[name].other}`);
-          oppositeButton.firstChild.src = `https://api.iconify.design/${conf[conf[name].other].icon}`;
+      button.id = `newButtons-${name}`;
+      button.style = "width: 24px; position: relative; top: -12px; margin-right: 20px;";
+      button.classList.remove("ytp-fullscreen-button");
+      button.setAttribute(
+        "title",
+        `${name[0].toUpperCase() + name.slice(1)}`
+      );
+      button.setAttribute("aria-label", name);
+      button.setAttribute("data-title-no-tooltip", name);
+      button.setAttribute("aria-keyshortcuts", DATA_BUTTONS[name].key);
 
-          original.click();
-        });
-        return button;        
-      }
-
-
-      function createNewButtons() {
-        const { buttonLike, buttonDisLike } = getButtons();
-        const right_controls = document.querySelector("div.ytp-right-controls");
-        const clonedButton = right_controls
-        .querySelector("button.ytp-fullscreen-button.ytp-button")
-        .cloneNode(true);        
-
-        let like = createButton("like", clonedButton, buttonLike);
-        let dislike = createButton("dislike", clonedButton, buttonDisLike);
-
-        right_controls.insertBefore(dislike, right_controls.firstChild, 'd');
-        right_controls.insertBefore(like, right_controls.firstChild, 'a');
-      }
-
-      createNewButtons();      
-
-      function OmitirButton() {
-        const boton = document.querySelector('.ytp-ad-skip-button-modern.ytp-button');
-        if (boton) {
-          boton.click();
-          console.log('Se ha hecho clic en el botón', new Date());
-        }
-      }
-
-      function OA() {
-        let interval = 3 * 1000; // 3 segundos
-        let clicker = setInterval(() => {
-          OmitirButton();
-        }, interval);        
-      }
-
-      OA();
-
-      function actions(method) {
-
-        const { buttonLike, buttonDisLike } = getButtons();
-        const methods = {
-          a: buttonLike,
-          d: buttonDisLike,
-          like: buttonLike,
-          dislike: buttonDisLike,
-        };
-        if (methods[method] && checkNoFocus()) {          
-          methods[method].click();          
-        }
-      }
-
-      document.addEventListener("keydown", (e) => {
-        actions(e.key);
+      button.addEventListener("click", () => {
+        console.log(name, original);
+        let icon = getAriaPressed(original) ? DATA_BUTTONS[name].icon : DATA_BUTTONS[name].icon_pressed;
+        ICON.src = `https://api.iconify.design/${icon}`;
+        oppositeButton = document.getElementById(`newButtons-${DATA_BUTTONS[name].other}`);
+        oppositeButton.firstChild.src = `https://api.iconify.design/${DATA_BUTTONS[DATA_BUTTONS[name].other].icon}`;
+        original.click();
       });
+      return button;
+    }
+
+
+    function createNewButtons() {
+      const { buttonLike, buttonDisLike } = getButtons();
+      const right_controls = document.querySelector("div.ytp-right-controls");
+      const clonedButton = right_controls
+        .querySelector("button.ytp-fullscreen-button.ytp-button")
+        .cloneNode(true);
+
+      let like = createButton("like", clonedButton, buttonLike);
+      let dislike = createButton("dislike", clonedButton, buttonDisLike);
+
+      right_controls.insertBefore(dislike, right_controls.firstChild);
+      right_controls.insertBefore(like, right_controls.firstChild);
+    }
+
+    let intervalButtons = setInterval(() => {
+      if (getButtons().buttonLike) {
+        createNewButtons();
+        clearInterval(intervalButtons);
+      }
+    }, 1000);
+
+    function OmitirButton() {
+      const boton = document.querySelector('.ytp-ad-skip-button-modern.ytp-button');
+      if (!boton) return;
+      setTimeout(() => {
+        boton.click();
+        console.log('Se ha omitido el anuncio', new Date());
+      }, 2000);
+    }
+
+    function observarCambiosEnDOM() {
+      let Elements = document.getElementsByTagName('ytd-watch-flexy');
+      let targetNode = Elements[0];
+      let config = { childList: true, subtree: true };
+      let observer = new MutationObserver(mutations => {
+        OmitirButton()
+      });
+      observer.observe(targetNode, config);
+    }
+
+    observarCambiosEnDOM();
+
+    function actions(method) {
+
+      const { buttonLike, buttonDisLike } = getButtons();
+      const methods = {
+        a: buttonLike,
+        d: buttonDisLike,
+        like: buttonLike,
+        dislike: buttonDisLike,
+      };
+      if (methods[method] && checkNoFocus()) {
+        methods[method].click();
+      }
+    }
+
+    document.addEventListener("keydown", (e) => {
+      actions(e.key);
     });
-
-  }, 3000); // tiempo de espera hasta que se cargue el DOM
-
+  }, 3500); // tiempo de espera hasta que se cargue el DOM
 };
 
 
